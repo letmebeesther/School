5d03d200d952372658e02913bc8ebdb05febdf2e
Esther Jheon <95136314+letmebeesther@users.noreply.github.com>
2024-12-08 14:28:01 +0900

    Add files via upload

---
+++ ASP/9주차/코드/DB 연결하여 자료 출력/3. DB연결하여 자료출력.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="3. DB연결하여 자료출력.aspx.cs" Inherits="week9._3_DB연결하여_자료출력" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div> <h2>학생 자료 찾기</h2>
+            <p>이름 :
+                <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            &nbsp;<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="찾기" />
+            </p>
+
+            <p>학번 : <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox> <br />
+               성별 : <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox> <br />
+               점수 : <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox> <br />
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP/9주차/코드/DB 연결하여 자료 출력/3. DB연결하여 자료출력.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace week9
+{
+    public partial class _3_DB연결하여_자료출력 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //1. 데이터 소스를 지정하는 연결 문자열 작성
+            string str = @"Data Source=localhost\SQLExpress; Initial Catalog=SampleDB; Integrated Security=True";
+
+            //2. SqlConnection 객체를 이용하여 DB연결
+            SqlConnection conn = new SqlConnection(str);
+
+            //3. 연결한 DB를 사용
+            conn.Open();
+            string sql = "select * from 성적 where 이름 = '"+TextBox1.Text+"'"; // SQL 명령문 
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            SqlDataReader reader = cmd.ExecuteReader(); // 명령문 실행
+
+            if (reader.Read()) // reader에 값이 있는지 확인
+            {
+                TextBox2.Text = reader["학번"].ToString();
+                TextBox3.Text = reader["성별"].ToString();
+                TextBox4.Text = reader["점수"].ToString();
+            }
+
+            reader.Close(); // 4. reader 인스턴스 종료
+            conn.Close(); // 5. DB 닫기 
+        }
+    }
+}
