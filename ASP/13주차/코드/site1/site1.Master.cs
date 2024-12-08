a58dff8a68b0fedbbe0e8d623c2a7a290ab89f09
Esther Jheon <95136314+letmebeesther@users.noreply.github.com>
2024-12-08 14:54:19 +0900

    Add files via upload

---
+++ ASP/13주차/코드/site1/site1.Master.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Security;
+
+namespace week_13
+{
+    public partial class site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.User.Identity.IsAuthenticated) // 인증된 사용자 확인
+            {
+                Label2.Text = Context.User.Identity.Name + "님"; // ~님
+                Panel1.Visible = false;
+                Panel2.Visible = true;
+            }
+            else
+            {
+                Panel1.Visible = true; // 로그인 panel을 보여주고
+                Panel2.Visible = false; // 로그아웃 panel을 감춤
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Login.aspx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Logout.aspx");
+        }
+    }
+}
