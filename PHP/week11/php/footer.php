f9389ba8eda22af444a10d9f7a87e2c05d2b580e
Esther Jheon <95136314+letmebeesther@users.noreply.github.com>
2024-12-08 15:45:15 +0900

    Delete PHP/week11 directory

--- PHP/week11/php/footer.php
+++
@@ -1,7 +0,0 @@
-        <div id="footer_content">
-            <p id="footer_logo">PHP 프로젝트 | <span>xxx프로젝트</span></p>
-            <ul id="author">
-                <li>저자 문의 메일</li>
-                <li>- 메일 주소 : pjy@baewha.ac.kr</li>
-            </ul>
-        </div>
