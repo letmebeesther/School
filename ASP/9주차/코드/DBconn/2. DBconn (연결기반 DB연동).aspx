d75375508e3ba8c8d807edf664e7b183f9cd307e
Esther Jheon <95136314+letmebeesther@users.noreply.github.com>
2024-12-08 14:28:31 +0900

    Add files via upload

---
+++ ASP/9주차/코드/DBconn/2. DBconn (연결기반 DB연동).aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="2. DBconn (연결기반 DB연동).aspx.cs" Inherits="week9._2_DBconn" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP/9주차/코드/DBconn/2. DBconn (연결기반 DB연동).aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace week9
+{
+    public partial class _2_DBconn : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //1. 데이터 소스를 지정하는 연결 문자열 작성
+            string str = @"Data Source=localhost\SQLExpress; Initial Catalog=SampleDB; Integrated Security=True";
+
+            //2. SqlConnection 객체를 이용하여 DB연결
+            SqlConnection conn = new SqlConnection(str);
+
+            //3. 연결한 DB를 사용
+            conn.Open();
+            string sql = "select * from 성적"; // SQL 명령문 
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            SqlDataReader reader = cmd.ExecuteReader(); // 명령문 실행
+
+            while (reader.Read()) // 결과 저장된 reader 인스턴스의 내용읽기 : 한 개 레코드 읽고 자동 다음 레코드
+            {
+                Response.Write(reader["이름"] + "(" + reader["점수"] + ")");
+                Response.Write("<br>");
+            }
+            reader.Close(); // 4. reader 인스턴스 종료
+            conn.Close(); // 5. DB 닫기
+        }
+    }
+}
