8151f17968d26d95a189eee74ef8132981375b3f
Esther Jheon <95136314+letmebeesther@users.noreply.github.com>
2024-12-08 15:38:41 +0900

    Add files via upload

---
+++ PHP/week3/echo_abb.php
@@ -0,0 +1,22 @@
+<?php
+	$id = "SmartIT";
+	$name = '박종열';
+	echo "아이디 : $id, 이름 : $name"
+	
+	//수정사항 : 화면 하단의 아이디, 이름이 출력되게 하시오
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta charset="UTF-8">
+<title>echo문 확장</title>
+</head>
+<body>
+	<div>
+		<h3>회원정보</h3>
+		<p>- 아이디 :          </p>
+		<p>- 이름 :           </p>
+	</div>
+</body>
+</html>		
